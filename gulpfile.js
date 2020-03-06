a3c977cf41efe45f2a512ef1f7a7812e00b60a3b
Markus Pfeifenberger <markus@webandco.com>
2020-03-06 12:10:13 +0100

    [task] update main gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -37,76 +37,73 @@ let distTasks = [];
 packages.forEach(function (theme) {
     let packageName = theme.name;
     let packagePath = theme.path + packageName;
+    let yamlConfigFile = packagePath + '/Configuration/Gulp.yaml' ;
 
-    let yamlConfig = readYaml(packagePath + '/Configuration/Gulp.yaml');
-    let yamlString = JSON.stringify(yamlConfig);
-    yamlString = yamlString.replace(/PACKAGE_PATH/g, packagePath);
-    yamlString = yamlString.replace(/PACKAGE_NAME/g, packageName);
-    yamlString = yamlString.replace(/PROJECT_ROOT/g, projectRoot);
+    if (isThere(yamlConfigFile)) {
+        let yamlConfig = readYaml(yamlConfigFile);
+        let yamlString = JSON.stringify(yamlConfig);
+        yamlString = replacePlaceholder(yamlString, packagePath, packageName, projectRoot);
 
-    // @deprecated - will be removed. Use PACKAGE_PATH and PACKAGE_NAME instead
-    yamlString = yamlString.replace(/THEME_PATH/g, packagePath);
-    yamlString = yamlString.replace(/THEME_NAME/g, packageName);
+        let config = JSON.parse(yamlString).Webandco.Gulp.config;
 
-    let config = JSON.parse(yamlString).Webandco.Gulp.config;
+        config.projectName = packageName;
+        config.taskPostfix = '-' + packageName.toLowerCase();
 
-    config.projectName = packageName;
-    // config.taskPostfix = '-' + packageName.toLowerCase().replace(/webco\.(\w+)\.theme/, "$1");
-    config.taskPostfix = '-' + packageName.toLowerCase();
+        let projectDistTasks = [
+            'dist-copy' + config.taskPostfix,
+            'dist-css' + config.taskPostfix,
+            'hint-js' + config.taskPostfix,
+            'dist-js' + config.taskPostfix,
+            'favicon' + config.taskPostfix,
+        ];
 
-    let projectDistTasks = [
-        'dist-copy' + config.taskPostfix,
-        'dist-css' + config.taskPostfix,
-        'hint-js' + config.taskPostfix,
-        'dist-js' + config.taskPostfix,
-        'favicon' + config.taskPostfix,
-    ];
+        browserSync[config.projectName] = require('browser-sync').create(config.projectName);
 
-    // console.log(config);
-    browserSync[config.projectName] = require('browser-sync').create(config.projectName);
+        for (let key in TASKS) {
+            require('./tasks/' + TASKS[key])({
+                config: config,
+                groupedTasks: taskGroups
+            });
+        }
 
-    for (let key in TASKS) {
-        require('./tasks/' + TASKS[key])({
-            config: config,
-            groupedTasks: taskGroups
-        });
+        // if (config.penthouse && config.penthouse.pages) {
+        //     penthouseExtractTaskGroups['penthouse-extract' + config.taskPostfix] = ['dist-css' + config.taskPostfix];
+        //     // console.log('penthouse', config.penthouse);
+        //     // console.log('pages', config.penthouse.pages);
+        //     require('./task/dist-penthouse')({
+        //         config: config,
+        //         groupedTasks: taskGroups,
+        //         gulp: gulp,
+        //         distTasks: penthouseDistTaskGroups,
+        //     });
+        //
+        //     for (let distId in config.penthouse.dist) {
+        //         // console.log(penthouseId, config.penthouse.dist[distId]);
+        //         require('./task/penthouse-combine')({
+        //             config: config,
+        //             groupedTasks: penthouseCombineTaskGroups,
+        //             distId: distId,
+        //             gulp: gulp
+        //         });
+        //     }
+        //
+        //     for (let penthouseId in config.penthouse.pages) {
+        //         // console.log(penthouseId, config.penthouse.pages[penthouseId]);
+        //         require('./task/penthouse-extract')({
+        //             config: config,
+        //             groupedTasks: penthouseExtractTaskGroups,
+        //             penthouseId: penthouseId,
+        //             gulp: gulp
+        //         });
+        //     }
+        //     projectDistTasks.push('dist-penthouse' + config.taskPostfix);
+        // }
+
+        gulp.task('dist' + config.taskPostfix, projectDistTasks)
+        distTasks.push('dist' + config.taskPostfix);
+    } else {
+        gulpUtil.log(gulpUtil.colors.gray(packageName + ' - Package has no Gulp.yaml file'));
     }
-
-    // if (config.penthouse && config.penthouse.pages) {
-    //     penthouseExtractTaskGroups['penthouse-extract' + config.taskPostfix] = ['dist-css' + config.taskPostfix];
-    //     // console.log('penthouse', config.penthouse);
-    //     // console.log('pages', config.penthouse.pages);
-    //     require('./task/dist-penthouse')({
-    //         config: config,
-    //         groupedTasks: taskGroups,
-    //         gulp: gulp,
-    //         distTasks: penthouseDistTaskGroups,
-    //     });
-    //
-    //     for (let distId in config.penthouse.dist) {
-    //         // console.log(penthouseId, config.penthouse.dist[distId]);
-    //         require('./task/penthouse-combine')({
-    //             config: config,
-    //             groupedTasks: penthouseCombineTaskGroups,
-    //             distId: distId,
-    //             gulp: gulp
-    //         });
-    //     }
-    //
-    //     for (let penthouseId in config.penthouse.pages) {
-    //         // console.log(penthouseId, config.penthouse.pages[penthouseId]);
-    //         require('./task/penthouse-extract')({
-    //             config: config,
-    //             groupedTasks: penthouseExtractTaskGroups,
-    //             penthouseId: penthouseId,
-    //             gulp: gulp
-    //         });
-    //     }
-    //     projectDistTasks.push('dist-penthouse' + config.taskPostfix);
-    // }
-
-    gulp.task('dist' + config.taskPostfix, projectDistTasks)
-    distTasks.push('dist' + config.taskPostfix);
 });
 
 if (distTasks.length > 0) {
