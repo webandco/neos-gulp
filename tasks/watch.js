3d01f14c9dc8618e01a65ce519dac6c3b1c911ef
Leo Preissegger <leo@webundco.com>
2020-04-23 16:48:36 +0200

    [task] fix crash in watch, if some things are not configured

--- tasks/watch.js
+++ tasks/watch.js
@@ -8,22 +8,22 @@ module.exports = function (opts) {
     addToTaskGroups(opts.groupedTasks, 'watch', opts.config.taskPostfix);
 
     gulp.task('watch' + opts.config.taskPostfix, function () {
-        if (opts.config.project.styles.bundled && opts.config.project.styles.bundled.watch)
+        if (opts.config.project.styles && opts.config.project.styles.bundled && opts.config.project.styles.bundled.watch)
             gulp.watch([opts.config.project.styles.bundled.watch], ['dist-css-bundle' + opts.config.taskPostfix]);
 
-        if (opts.config.project.styles.fusion && opts.config.project.styles.fusion.watch)
+        if (opts.config.project.styles && opts.config.project.styles.fusion && opts.config.project.styles.fusion.watch)
             gulp.watch([opts.config.project.styles.fusion.watch], ['dist-css-fusion' + opts.config.taskPostfix, opts.browserSync.reload]);
 
-        if (opts.config.project.scripts.bundled && opts.config.project.scripts.bundled.watch)
+        if (opts.config.project.scripts && opts.config.project.scripts.bundled && opts.config.project.scripts.bundled.watch)
             gulp.watch([opts.config.project.scripts.bundled.watch], ['dist-js-bundle' + opts.config.taskPostfix]);
 
-        if (opts.config.project.scripts.fusion && opts.config.project.scripts.fusion.watch)
+        if (opts.config.project.scripts && opts.config.project.scripts.fusion && opts.config.project.scripts.fusion.watch)
             gulp.watch([opts.config.project.scripts.fusion.watch], ['dist-js-fusion' + opts.config.taskPostfix, opts.browserSync.reload]);
 
-        if (opts.config.project.scripts.serviceWorker && opts.config.project.scripts.serviceWorker.source)
+        if (opts.config.project.scripts && opts.config.project.scripts.serviceWorker && opts.config.project.scripts.serviceWorker.source)
             gulp.watch([opts.config.project.scripts.serviceWorker.source], ['dist-serviceworker' + opts.config.taskPostfix, opts.browserSync.reload]);
 
-        if (opts.config.project.scripts.lint && opts.config.project.scripts.lint.sources)
+        if (opts.config.project.scripts && opts.config.project.scripts.lint && opts.config.project.scripts.lint.sources)
             gulp.watch([opts.config.project.scripts.lint.sources], ['lint-js' + opts.config.taskPostfix]);
 
         if (opts.config.project.styles && opts.config.project.styles.lint)
